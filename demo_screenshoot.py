7752c909a193152fa55f5ef18ea10d22ce919f41
Rival Muhammad Sayid <rivalmsayid@gmail.com>
2024-11-30 17:04:01 +0700

    add demo_screenshoot

---
+++ demo_screenshoot.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+driver.get("https://www.tokopedia.com/")
+driver.get_screenshot_as_file("tokped.png")
